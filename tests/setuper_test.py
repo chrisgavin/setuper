077d8d5be12032587073923d9261eaa09c5da889
Chris Gavin <chris@chrisgavin.me>
2020-06-14 18:55:17 +0100

    Refactor tests to use a common test method.

--- tests/setuper_test.py
+++ tests/setuper_test.py
@@ -13,25 +13,22 @@ def _get_installed_packages(virtualenv_path):
 	installed_packages = {package for package in installed_packages if package}
 	return installed_packages
 
-def test_setup():
-	setup_path = pathlib.Path(__file__).parent / "resources" / "setup.py"
+def _test_with_test_file(test_file_name, expected_packages=set(), unexpected_packages=set(), **kwargs):
+	setup_path = pathlib.Path(__file__).parent / "resources" / test_file_name
 	with tempfile.TemporaryDirectory("setuper_tests") as test_virtualenv:
 		virtualenv_path = pathlib.Path(test_virtualenv)
 		virtualenv.cli_run([str(virtualenv_path)])
 		initial_packages = _get_installed_packages(virtualenv_path)
-		setuper.run(setup_path, pip=[str(virtualenv_path / "bin" / "python"), "-m", "pip"])
+		setuper.run(setup_path, pip=[str(virtualenv_path / "bin" / "python"), "-m", "pip"], **kwargs)
 		final_packages = _get_installed_packages(virtualenv_path)
 		packages = final_packages.difference(initial_packages)
-		assert {"Jinja2", "requests"}.issubset(packages)
+		if expected_packages:
+			assert expected_packages.issubset(packages)
+		if unexpected_packages:
+			assert not unexpected_packages.issubset(packages)
+
+def test_setup():
+	_test_with_test_file("setup.py", expected_packages={"Jinja2", "requests"})
 
 def test_extras_require():
-	setup_path = pathlib.Path(__file__).parent / "resources" / "extras_require.py"
-	with tempfile.TemporaryDirectory("setuper_tests") as test_virtualenv:
-		virtualenv_path = pathlib.Path(test_virtualenv)
-		virtualenv.cli_run([str(virtualenv_path)])
-		initial_packages = _get_installed_packages(virtualenv_path)
-		setuper.run(setup_path, pip=[str(virtualenv_path / "bin" / "python"), "-m", "pip"], extras=["dev"])
-		final_packages = _get_installed_packages(virtualenv_path)
-		packages = final_packages.difference(initial_packages)
-		assert {"Jinja2", "requests", "Pillow"}.issubset(packages)
-		assert not {"matplotlib"}.issubset(packages)
+	_test_with_test_file("extras_require.py", expected_packages={"Jinja2", "requests", "Pillow"}, unexpected_packages={"matplotlib"}, extras=["dev"])
